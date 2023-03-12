e31149a3f1f47a026835888840d97ce697a5c144
Vibhabakkam <vibhabakkam22@gmail.com>
2023-03-12 10:16:41 +0530

    updates

--- product.js
+++ product.js
@@ -1,38 +1,42 @@
-window.onload = function(){
-    // alert("worked")
-    var dataFromLS = JSON.parse(localStorage.getItem("ajiouserProduct"));
-    console.log(dataFromLS,"dataFromLS")
-    
-
-    array = [];
-    for(var i=0; i<dataFromLS.length; i++){
-        array += `<div id = "picture2"><img src="${ dataFromLS[i].proImg}" /><h1>${dataFromLS[i].proName}</h1><p>${dataFromLS[i].proPrice}</p><button id = "cartbutton" onclick='addToCart(${JSON.stringify(dataFromLS[i])})' >Add to cart</button></div>`
-        // console.log(dataFromLS[i].proName)
+window.onload = function () {
+    var divFromHtml = document.getElementById("product");
+    var products = JSON.parse(localStorage.getItem("ajiouserproduct"));
+  
+    var array = [];
+  
+    for (var i = 0; i < products.length; i++) {
+      // console.log(products[i]);
+      array += `<div id = "picture2"><img src="${products[i].image}" alt="productImg" /><h2>${products[i].name}</h2><p>${products[i].price}</p><button id = "cartbutton" onclick='addToCart(${JSON.stringify(products[i])})'>Add to cart</button></div>`;
     }
-    var divFromHTML = document.getElementById("screen")
-    divFromHTML.innerHTML = array;
-}
-
-function addToCart(pro){
-    var product = JSON.stringify(pro);
-    var dataFromLS = JSON.parse(localStorage.getItem("ajiouserData"));
-    var currentUser = JSON.parse(localStorage.getItem("currentUser"));
-    if (currentUser){
-        var allUsers = []
-        for (var i=0; i <dataFromLS.length ; i++){
-            if(dataFromLS[i].email === currentUser["User"]){
-                var newObj = dataFromLS[i];
-                newObj["cartProducts"] = newObj["cartProducts"] || [];
-                newObj["cartProducts"].push(JSON.parse(product));
-                allUsers.push(newObj)
-            } else {
-                allUsers.push(dataFromLS[i])
-            }
-        }
-        localStorage.setItem("ajiouserData", JSON.stringify(allUsers));
-        alert("product added to cart ")
-    } else{
-        alert(" login first to add product")
-    }
-
-}
+  
+    divFromHtml.innerHTML = array;
+  };
+  
+  
+  function addToCart(pro){
+      alert("worked");
+      var ajiouserproduct =JSON.stringify(pro);
+      var dataFromLs =JSON.parse(localStorage.getItem("ajiouserData"));
+      var currentUser =JSON.parse(localStorage.getItem("currentUser"));
+  
+      if(currentUser){
+          var allUsers =[];
+          for( var i=0; i<dataFromLs.length; i++){
+              if(dataFromLs[i].email === currentUser["current-user-email"]){
+                  var newObj = dataFromLs[i];
+                  newObj["cartProducts"] = newObj["cartProducts"] || [];
+                  newObj["cartProducts"].push(JSON.parse(ajiouserproduct));
+                  allUsers.push(newObj);
+              }
+              else{
+                  allUsers.push(dataFromLs[i]);
+              }
+          }
+          // console.log()
+          localStorage.setItem("ajiouserData",JSON.stringify(allUsers));
+          alert("Product added to cart");
+      }
+      else{
+          alert("login first to add product into cart");
+      }
+  }
--- server.js
+++ server.js
@@ -1,136 +1,138 @@
-function register(event) {
-    // getting data from html to js
+function register(event){
     event.preventDefault();
-    var userName = document.getElementById("userName").value;
-    var userPhone = document.getElementById("userPhone").value;
-    var userEmail = document.getElementById("userEmail").value;
-    var userPassword = document.getElementById("userPassword").value;
-    var ajiouserData = { name: userName, number : userPhone, email: userEmail, password: userPassword }
 
-    // storing data from js to ls
-    var dataFromLS = JSON.parse(localStorage.getItem("ajiouserData")) || [];
-    console.log(dataFromLS, 'dataFromLS')
-    var flag = false;
-    for (var i = 0; i < dataFromLS.length; i++) {
-        if (dataFromLS[i].email === userEmail) {
-            flag = true;
+    var userName= document.getElementById("userName").value;
+    var userEmail =document.getElementById("userEmail").value;
+    var userPassword =document.getElementById("userPassword").value;
+    var ajiouserData = { name:userName, email:userEmail, password:userPassword };
+
+    var dataFromLs =JSON.parse(localStorage.getItem("ajiouserData")) || [];
+    console.log(dataFromLs,"dataFromLs");
+
+    var flag =false;
+    for(var i=0; i<dataFromLs.length;i++){
+        if(dataFromLs[i].email === userEmail){
+            flag =true;
         }
     }
-    if (flag === true) {
-        alert("Email already present, use another one")
-    } else if (userPassword.length < 1 && userName.length < 1 && userEmail.length < 1) {
-        alert("must fill all fields")
-    } else if (userPassword.length < 8) {
-        alert("password must be more than 8 digit")
-    } else {
-        dataFromLS.push(ajiouserData);
-        localStorage.setItem("ajiouserData", JSON.stringify(dataFromLS));
-        document.getElementById("userName").value = " ";
-        document.getElementById("userPhone").value = " ";
-        document.getElementById("userEmail").value = " ";
-        document.getElementById("userPassword").value = " ";
-        window.location.href = "/ajiologin.html";
-        alert("Registration done");
 
+    if (flag === true){
+        alert("Email already Present");
+        document.getElementById("userEmail").value ='';
+    }
+    else if(userName.length < 1 && userEmail.length <1){
+        alert("must filled all fields");
+    }
+    else if(userPassword.length < 8){
+        alert("Password Must be more than 8 digit");
+        document.getElementById("userPassword").value ="";
+    }
+    else{
+        dataFromLs.push(ajiouserData);
+        localStorage.setItem("ajiouserData",JSON.stringify(dataFromLs));
+        document.getElementById("userName").value ='';
+        document.getElementById("userEmail").value ='';
+        document.getElementById("userPassword").value ="";
+        window.location.href ="/ajiologin.html";
+        alert("registration Done");
     }
 }
 
-function login(event) {
-    event.preventDefault();
-    var userEmail = document.getElementById("email").value;
+function login(event){
+    event.preventDefault(event);
+    var userEmail =document.getElementById("email").value;
     var userPassword = document.getElementById("password").value;
-    var ajiouserData = { email: userEmail, password: userPassword }
 
-    var dataFromLS = JSON.parse(localStorage.getItem("ajiouserData")) || [];
-    console.log(dataFromLS, 'dataFromLS')
-    var flag = false;
-    for (var i = 0; i < dataFromLS.length; i++) {
-        if (dataFromLS[i].email === userEmail && dataFromLS[i].password === userPassword) {
-            flag = true;
+    var dataFromLs =JSON.parse(localStorage.getItem("ajiouserData"));
+
+    var flag= false;
+
+    for (var i=0 ; i< dataFromLs.length ; i++){
+        if(dataFromLs[i].email === userEmail  && dataFromLs[i].password === userPassword){
+            flag =true;
         }
     }
-    if (flag === true) {
-        alert("login succecfully")
-        dataFromLS.push(ajiouserData);
-        localStorage.setItem("ajiouserData", JSON.stringify(dataFromLS));
-        document.getElementById("email").value = " ";
-        document.getElementById("password").value = " ";
-        var User = {};
-        User = { email: userEmail }
-        localStorage.setItem("currentUser", JSON.stringify(User));
 
+    if(flag){
+        document.getElementById("email").value ="";
+        document.getElementById("password").value ="";
 
-        window.location.href = '/ajio.html';
-    } else {
-
-        alert("Wrong cred, Please check your email and password");
+        var user ={};
+        user["current-user-email"]= userEmail;
+        localStorage.setItem("currentUser",JSON.stringify(user));
+        window.location.href="/ajio.html";
+        alert("Login sucessful");
     }
-}
+    else{
+        alert("Email or Password does not match");
+    }
 
+}
 var gettingEmail;
-function forgetPassword() {
 
-    var dataFromLS = JSON.parse(localStorage.getItem("ajiouserData"));
-    var userEmail = document.getElementById("email").value;
-    gettingEmail = userEmail;
+function forgotPassword(event){
+    event.preventDefault();
+    
+    var dataFromLs=JSON.parse(localStorage.getItem("ajiouserData"));
+    var emailFromUser =document.getElementById("email").value;
+
+    gettingEmail =emailFromUser;
 
     var flag = false;
-    for (var i = 0; i < dataFromLS.length; i++) {
-        if (dataFromLS[i].email === userEmail) {
-            flag = true;
+    for(var i=0; i<dataFromLs.length; i++){
+        if(dataFromLs[i].email === emailFromUser){
+            flag =true;
         }
     }
-    if (flag === true) {
-        // window.location.href = '/newpass.html';
+    if(flag){
+        var newCode =`<input type="password" id="password"><br><button onClick="newPassword()"Reset> New Password</button>`;
+        var divFromHtml =document.getElementById("change");
+        divFromHtml.innerHTML =newCode;
+        alert("Now set New Password");
 
-        var newCode = `<input type = "password" id ="password" > <br> <button id="regdiv" onclick="newPassword()">set new password</button>`
-        var divFromHtml = document.getElementById("change")
-        divFromHtml.innerHTML = newCode;
-        alert(" now set new password")
     }
-    else {
-        alert(" email not found ,check email again ")
+    else{
+        alert("Please Enter Register Email");
+        document.getElementById("email").value ="";
     }
-
-
 }
 
+function newPassword(){
+    
+    var dataFromLs =JSON.parse(localStorage.getItem("ajiouserData"));
+    var userPassword =document.getElementById("password").value;
 
-function newPassword() {
-    var userPassword = document.getElementById("password").value;
-    var dataFromLS = JSON.parse(localStorage.getItem("ajiouserData"));
-    //    alert ("worked")
-
-
-    for (var i = 0; i < dataFromLS.length; i++) {
-        if (dataFromLS[i].email === gettingEmail) {
-            dataFromLS[i].password = userPassword
+    // console.log(passwordFromUser,"Password");
+    for(var i=0; i<dataFromLs.length; i++){
+        if(dataFromLs[i].email === gettingEmail){
+            dataFromLs[i].password = userPassword;
         }
     }
-    localStorage.setItem('ajiouserData', JSON.stringify(dataFromLS));
-    gettingEmail = "";
+     // console.log(passwordFromUser,"Password");
 
-    window.location.href = '/ajiologin.html';
-    alert("password change now login")
+    localStorage.setItem("ajiouserData",JSON.stringify(dataFromLs));
+    gettingEmail ="";
+    // divFromHtml.innerHTML =rest;
+    window.location.href ="/ajiologin.html";
+    alert("Password Changed , Now Login");
 
+    
 }
 
-function addToLS() {
-    alert("product added")
-    var Name = document.getElementById("proName").value;
-    var Imge = document.getElementById("proImg").value;
-    var Price = document.getElementById("proPrice").value;
+function addToLS(){
 
-    var ajiouserProduct = {  proImg:Imge, proName: Name, proPrice:Price }
-    // store deta from java script to local storage
-    var dataFromLS = JSON.parse(localStorage.getItem("ajiouserProduct")) || [];
-    console.log(dataFromLS, 'dataFromLS')
-  
-    dataFromLS.push(ajiouserProduct);
-    localStorage.setItem("ajiouserProduct", JSON.stringify(dataFromLS));
-    alert("product added")
+    var dataFromLs =JSON.parse(localStorage.getItem("ajiouserproduct")) || [];
 
-    var Name = document.getElementById("proName").value ="" ;
-    var Imge = document.getElementById("proImg").value ="";
-    var Price = document.getElementById("proPrice").value ="";
+    var userName = document.getElementById("name").value;
+    var userImage= document.getElementById("image").value;
+    var price =document.getElementById("price").value;
+
+    var ajiouserproduct ={name:userName, image:userImage, price:price};
+    dataFromLs.push(ajiouserproduct);
+
+    localStorage.setItem("ajiouserproduct",JSON.stringify(dataFromLs));
+    alert("Added");
+    var userName = document.getElementById("name").value = "";
+    var userImage= document.getElementById("image").value = "";
+    var price =document.getElementById("price").value = "";
 }
